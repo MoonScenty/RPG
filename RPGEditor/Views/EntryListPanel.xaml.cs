063379ab43ca157b874af6e238e3546489ff0b4d
MoonScenty <silvia_s@naver.com>
2026-08-01 03:53:47 +0900

    애니메이션/상태 목록에 ID 표시
    - EntryListPanel에 ShowId 옵션 추가, 애니메이션/상태 탭에서 유형 탭처럼 목록에 ID를 함께 표시
    - 상세 화면 상단에 중복 표시되던 ID 텍스트 제거

--- RPGEditor/Views/EntryListPanel.xaml.cs
+++ RPGEditor/Views/EntryListPanel.xaml.cs
@@ -1,9 +1,20 @@
+using System.Windows;
 using System.Windows.Controls;
 
 namespace RPGEditor.Views;
 
 public partial class EntryListPanel : UserControl
 {
+    public static readonly DependencyProperty ShowIdProperty =
+        DependencyProperty.Register(nameof(ShowId), typeof(bool), typeof(EntryListPanel), new PropertyMetadata(false));
+
+    /// <summary>true면 목록 각 항목 앞에 ID를 함께 표시한다.</summary>
+    public bool ShowId
+    {
+        get => (bool)GetValue(ShowIdProperty);
+        set => SetValue(ShowIdProperty, value);
+    }
+
     public EntryListPanel()
     {
         InitializeComponent();
