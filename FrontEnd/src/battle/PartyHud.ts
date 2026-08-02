9e17cd14874ca672bf34a42dd2400a3818b1a794
MoonScenty <silvia_s@naver.com>
2026-08-02 21:58:34 +0900

    HP 수치 왼쪽으로 100px 추가 이동(사용자 지시, 누적 150px)

--- FrontEnd/src/battle/PartyHud.ts
+++ FrontEnd/src/battle/PartyHud.ts
@@ -142,8 +142,8 @@ export class PartyHud {
     // 위치는 그대로 두고 스타일(크기/그라디언트/테두리)과 정렬만 덮어씀(사용자 지시).
     this.applyBigValueStyle(hp.valueText, HP_VALUE_GRADIENT_BOTTOM, 0)
     this.applyBigValueStyle(mp.valueText, MP_VALUE_GRADIENT_BOTTOM, 1)
-    // 사용자 지시로 왼쪽 50px, 위로 5px 이동.
-    hp.valueText.position.x -= 50
+    // 사용자 지시로 왼쪽 50px + 100px(누적 150px), 위로 5px 이동.
+    hp.valueText.position.x -= 150
     hp.valueText.position.y -= 5
     // 사용자 지시로 왼쪽 10px, 위로 5px 이동.
     mp.valueText.position.x -= 10
